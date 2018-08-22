e8ed34c7efa30bd69029edb3a9c6806142565c90
TKU Course Application <tku.course.assistant@gmail.com>
2018-08-22 23:42:32 +0800

    update index.php

--- web/LINEBotTiny.php
+++ web/LINEBotTiny.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright 2016 LINE Corporation
  *
@@ -14,6 +15,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+
 /*
  * This polyfill of hash_equals() is a modified edition of https://github.com/indigophp/hash-compat/tree/43a19f42093a0cd2d11874dff9d891027fc42214
  *
@@ -23,19 +25,24 @@
  */
 if (!function_exists('hash_equals')) {
     defined('USE_MB_STRING') or define('USE_MB_STRING', function_exists('mb_strlen'));
+
     function hash_equals($knownString, $userString)
     {
         $strlen = function ($string) {
             if (USE_MB_STRING) {
                 return mb_strlen($string, '8bit');
             }
+
             return strlen($string);
         };
+
         // Compare string lengths
         if (($length = $strlen($knownString)) !== $strlen($userString)) {
             return false;
         }
+
         $diff = 0;
+
         // Calculate differences
         for ($i = 0; $i < $length; $i++) {
             $diff |= ord($knownString[$i]) ^ ord($userString[$i]);
@@ -43,6 +50,7 @@ if (!function_exists('hash_equals')) {
         return $diff === 0;
     }
 }
+
 class LINEBotTiny
 {
     public function __construct($channelAccessToken, $channelSecret)
@@ -50,6 +58,7 @@ class LINEBotTiny
         $this->channelAccessToken = $channelAccessToken;
         $this->channelSecret = $channelSecret;
     }
+
     public function parseEvents()
     {
         if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
@@ -57,17 +66,21 @@ class LINEBotTiny
             error_log("Method not allowed");
             exit();
         }
+
         $entityBody = file_get_contents('php://input');
+
         if (strlen($entityBody) === 0) {
             http_response_code(400);
             error_log("Missing request body");
             exit();
         }
+
         if (!hash_equals($this->sign($entityBody), $_SERVER['HTTP_X_LINE_SIGNATURE'])) {
             http_response_code(400);
             error_log("Invalid signature value");
             exit();
         }
+
         $data = json_decode($entityBody, true);
         if (!isset($data['events'])) {
             http_response_code(400);
@@ -76,12 +89,14 @@ class LINEBotTiny
         }
         return $data['events'];
     }
+
     public function replyMessage($message)
     {
         $header = array(
             "Content-Type: application/json",
             'Authorization: Bearer ' . $this->channelAccessToken,
         );
+
         $context = stream_context_create(array(
             "http" => array(
                 "method" => "POST",
@@ -89,12 +104,14 @@ class LINEBotTiny
                 "content" => json_encode($message),
             ),
         ));
+
         $response = file_get_contents('https://api.line.me/v2/bot/message/reply', false, $context);
         if (strpos($http_response_header[0], '200') === false) {
-            http_response_code(500);
+            #http_response_code(500);
             error_log("Request failed: " . $response);
         }
     }
+
     private function sign($body)
     {
         $hash = hash_hmac('sha256', $body, $this->channelSecret, true);
--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright 2016 LINE Corporation
  *
@@ -14,9 +15,14 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+
 require_once('./LINEBotTiny.php');
-$channelAccessToken = 'U6++id4mj547Zzka/StPWHxE9TWU3YLRoW5UMocdfWEOxnbqO6NIwCLU8/nZD8xdAxdH9spwnRFfPAHo5vRCqO79Avlpl84U+y35LgL6+cFRZ6vfbhOa+n8gF9K+3EY1wIg9/+k5FNPcPE0bwJaIxwdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '996ae268eeff17dbd5bb6693e69a880c';
+
+
+
+$channelAccessToken = getenv('U6++id4mj547Zzka/StPWHxE9TWU3YLRoW5UMocdfWEOxnbqO6NIwCLU8/nZD8xdAxdH9spwnRFfPAHo5vRCqO79Avlpl84U+y35LgL6+cFRZ6vfbhOa+n8gF9K+3EY1wIg9/+k5FNPcPE0bwJaIxwdB04t89/1O/w1cDnyilFU=');
+$channelSecret = getenv('996ae268eeff17dbd5bb6693e69a880c');
+
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
@@ -24,19 +30,21 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-                    $client->replyMessage(array(
+                	$m_message = $message['text'];
+                	if($m_message!="")
+                	{
+                		$client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => $message['text']
+                                'text' => $m_message
                             )
                         )
-                    ));
+                    	));
+                	}
                     break;
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
+                
             }
             break;
         default:
