f6344db3e00586139f0e563987caa80c94c40d0d
TKU Course Application <tku.course.assistant@gmail.com>
2018-08-22 17:22:48 +0800

    update index.php

--- index.php
+++ index.php
@@ -14,37 +14,33 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
- 
 require_once('./LINEBotTiny.php');
 $channelAccessToken = 'U6++id4mj547Zzka/StPWHxE9TWU3YLRoW5UMocdfWEOxnbqO6NIwCLU8/nZD8xdAxdH9spwnRFfPAHo5vRCqO79Avlpl84U+y35LgL6+cFRZ6vfbhOa+n8gF9K+3EY1wIg9/+k5FNPcPE0bwJaIxwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '996ae268eeff17dbd5bb6693e69a880c';
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
-//check user is exsist begin
     switch ($event['type']) {
         case 'message':
-		    $message = $event['message'];
-			
+            $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-				    $client->replyMessage(array(
+                    $client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
                         'messages' => array(
-							array(
+                            array(
                                 'type' => 'text',
-                                'text' => '別生氣'
-                            ),
-							array(
-                                'type' => 'text',
-                                'text' => '別不爽'
+                                'text' => $message['text']
                             )
-							
-						)
-					));
-				
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
             }
             break;
-		
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
     }
 };
